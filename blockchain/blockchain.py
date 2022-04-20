3707f5ec8c0e843c2ab33c97ff43e0260a2852bb
LoneCannibal <aakashsd2000@gmail.com>
2022-04-20 15:31:51 +0530

    Built blockchain fundamentals

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+from time import time
+
+
+
 
 class Blockchain:
     
     def __init__(self):
         self.transactions = []
         self.chain = []
+        #Create genesis block
+        self.create_block(0,'0'*64)
+    
+
+    #Add a block to the blockchain
+    def create_block(self, nonce, previous_hash):
+        block = {   
+            'block_number': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.transactions,
+            'nonce': nonce,
+            'previous_hash': previous_hash
+        }
+        #Reset current list of transactions
+        self.transactions = []
+        self.chain.append(block)
+
 
 #Instantiate blockchain
 blockchain = Blockchain()
@@ -14,7 +35,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return './index.html'
+    return render_template('./index.html')
 
 
 if __name__ == '__main__':
